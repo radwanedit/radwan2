1ebd35114ea11a0be0a5a1c34fae25929b72a885
radwanedit <46229334+radwanedit@users.noreply.github.com>
2018-12-29 00:33:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', '🎆welcome🎆');
-    let memberavatar = member.user.avatarURL
-      if (!channel) return; 
-    let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
-        .addField(':loudspeaker: | اهلا بك في سيرفرنا شرفتنا يا حبي', `Welcome tاهلا بك في سيرفرنا شرفتنا${member}`)
-        .addField('User ID :', `"[" + ${member.id} + "]"` )
-                .addField(':arrow_down:',`** انت العضو رقم ${member.guild.memberCount}**`)
-
-                  .addField("Name:",`**<@${member.id}>**`, true)
-
-                                     .addField(' RMY bot', `${member.guild.name}`,true)
-
-     .setFooter('PC ARMY bot')
-        .setTimestamp()
-
-      channel.sendEmbed(embed);
-});
-
- client.on('guildMemberRemove', member => {
-    var embed = new Discord.RichEmbed()
-.setAuthor(member.user.username, member.user.avatarURL)
-.setThumbnail(member.user.avatarURL)
-.setTitle('***لماذا خرجت من السيرفر يا صديقي فضلا و ليس امرا ارجع***')
-.addField('**الاسم**',`[ ${member} ]`)
-.addField('**عدد الاعضاء**',`[ ${member.guild.memberCount} ]`,true)
-.setColor('Random')
-   
-   var channel =member.guild.channels.find('name', '🎆welcome🎆')
-   if (!channel) return;
-         channel.send({embed : embed});
- 
-});
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });

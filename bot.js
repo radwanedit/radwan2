e84eca30fbc177e51d52e4864c3fd1f02c7f2a69
radwanedit <46229334+radwanedit@users.noreply.github.com>
2018-12-29 00:35:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
   if (msg.content === 'هلا') {
-    msg.reply('اهلا بيك شرفت!');
+    msg.reply('هلا بك شرفت بتمني انك تتفاعل!');
   }
 });
+
 client.login(process.env.BOT_TOKEN);
